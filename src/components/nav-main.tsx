30a2019f348ee2c5259335421fbeb3d386278e45
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-11-18 23:50:27 +0100

    removed -dom

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,4 +1,4 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link } from "react-router";
 import { ChevronLeft, type LucideIcon } from "lucide-react";
 
 import {
--- src/pages/about/sections/project.tsx
+++ src/pages/about/sections/project.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { FaWrench, FaArrowRight } from "react-icons/fa6";
 
 import { Button } from "@/components/ui/button";
