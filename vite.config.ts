cbb6c4d733ba3c0050dd7425c38a911f960ccb2a
Nevò Mirzai Hamadani <140847350+NevoMir@users.noreply.github.com>
2025-11-18 23:04:59 +0100

    vite.config.ts changes to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/simpleplain/",
+    base: "/Nevo-Mirzai-Website/",
     plugins: [react(), tailwindcss()],
     assetsInclude: ["**/*.pptx", "**/*.ppt"],
     resolve: {
